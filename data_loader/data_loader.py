647c6b7ddfdbdfa165ad07d62ab510f71aafd39f
MartinaKolibasova <martina.kolibasova1@gmail.com>
2020-05-10 21:19:48 +0200

    Add data loader

---
+++ data_loader/data_loader.py
@@ -0,0 +1,47 @@
+import glob
+import os
+import SimpleITK as sitk
+import sys
+from torch.utils.data import Dataset
+
+
+class ImageNiftiDataset(Dataset):
+    """.nii images dataset"""
+
+    def __init__(self, data_root):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+        """
+        self.data_root = data_root
+        self.samples = []
+        self.saliences = []
+        self.segmentations = []
+
+        # all data in folder
+        for mri in os.listdir(data_root):
+            mri_folder = os.path.join(data_root, mri)
+
+            files = glob.glob(mri_folder + "/*.nii")
+            if len(files) > 0:
+                for file in files:
+                    print(file)
+                    if file.split("/")[1] == "sdata.nii":
+                        self.samples.append(sitk.ReadImage(files[0]))
+                    elif file.split("/")[1] == "saliency.nii":
+                        self.saliences.append(sitk.ReadImage(files[0]))
+                    elif file.split("/")[1] == "segmentation.nii":
+                        self.segmentations.append(sitk.ReadImage(files[0]))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        return self.samples[idx]
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 0:
+        dataset = ImageNiftiDataset(str(sys.argv) + '/')
+    else:
+        dataset = ImageNiftiDataset('data/')
